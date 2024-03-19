7560745612c7079363fdb0358f3c438100103811
vicg0 <victor.msantos@sptech.school>
2024-03-19 00:17:02 -0300

    adicionei descrições e fotos no carrousel

--- SiteInstitucional/js/carrosel.js
+++ SiteInstitucional/js/carrosel.js
@@ -1,36 +1,41 @@
 var members = [
     {
-        name: "Victor",
+        name: "Amanda",
         category: "Front-End",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie mi eu aliquet ullamcorper. Pellentesque rutrum ante urna, nec finibus odio tempor sagittis. Aliquam vitae mollis tellus. Maecenas pharetra bibendum venenatis. Mauris quis placerat elit. Nulla vitae condimentum felis. In luctus a lorem a ultrices. Duis viverra, odio eget molestie mattis, dui leo accumsan dui, interdum auctor velit elit tincidunt leo."
+        description: "Desde o começo da minha jornada em tecnologia, sempre fui apaixonada pela área do Front-End. Gosto de me desafiar e aprender coisas novas e atualmente estou me aventurando na área de Dados.",
+        photo: 'url(./assets/images/guedes.png)'
     },
     {
         name: "Carlos",
         category: "Full-Stack",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie mi eu aliquet ullamcorper. Pellentesque rutrum ante urna, nec finibus odio tempor sagittis. Aliquam vitae mollis tellus. Maecenas pharetra bibendum venenatis. Mauris quis placerat elit. Nulla vitae condimentum felis. In luctus a lorem a ultrices. Duis viverra, odio eget molestie mattis, dui leo accumsan dui, interdum auctor velit elit tincidunt leo."
-    },
-    {
-        name: "Kely",
-        category: "Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie mi eu aliquet ullamcorper. Pellentesque rutrum ante urna, nec finibus odio tempor sagittis. Aliquam vitae mollis tellus. Maecenas pharetra bibendum venenatis. Mauris quis placerat elit. Nulla vitae condimentum felis. In luctus a lorem a ultrices. Duis viverra, odio eget molestie mattis, dui leo accumsan dui, interdum auctor velit elit tincidunt leo."
-    },
-    {
-        name: "Amanda",
-        category: "Front-End",
-        description: "Desde o começo da minha jornada em tecnologia, sempre fui apaixonada pela área do Front-End. Gosto de me desafiar e aprender coisas novas e atualmente estou me aventurando na área de Dados."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie mi eu aliquet ullamcorper. Pellentesque rutrum ante urna, nec finibus odio tempor sagittis. Aliquam vitae mollis tellus. Maecenas pharetra bibendum venenatis. Mauris quis placerat elit. Nulla vitae condimentum felis. In luctus a lorem a ultrices. Duis viverra, odio eget molestie mattis, dui leo accumsan dui, interdum auctor velit elit tincidunt leo.",
+        photo: 'url(./assets/images/carlos.png)'
     },
     {
         name: "Gabriel",
         category: "Back-End",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie mi eu aliquet ullamcorper. Pellentesque rutrum ante urna, nec finibus odio tempor sagittis. Aliquam vitae mollis tellus. Maecenas pharetra bibendum venenatis. Mauris quis placerat elit. Nulla vitae condimentum felis. In luctus a lorem a ultrices. Duis viverra, odio eget molestie mattis, dui leo accumsan dui, interdum auctor velit elit tincidunt leo."
+        description: "Acredito que o único jeito de se desenvolver como pessoa é aperfeiçoar a capacidade de auxiliar os outros. Dessa forma, a LifeLine trabalha para ajudar as pessoas e, somente dessa forma, avaliamos nosso progresso.",
+        photo: 'url(./assets/images/guedes.png)'
+    },
+    {
+        name: "Kely",
+        category: "Design",
+        description: "A criatividade sempre andou lado a lado e é o que entiquecesse nosso projeto",
+        photo: 'url(./assets/images/kely.png)'
     },
     {
         name: "Pedro",
         category: "Back-End",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie mi eu aliquet ullamcorper. Pellentesque rutrum ante urna, nec finibus odio tempor sagittis. Aliquam vitae mollis tellus. Maecenas pharetra bibendum venenatis. Mauris quis placerat elit. Nulla vitae condimentum felis. In luctus a lorem a ultrices. Duis viverra, odio eget molestie mattis, dui leo accumsan dui, interdum auctor velit elit tincidunt leo."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie mi eu aliquet ullamcorper. Pellentesque rutrum ante urna, nec finibus odio tempor sagittis. Aliquam vitae mollis tellus. Maecenas pharetra bibendum venenatis. Mauris quis placerat elit. Nulla vitae condimentum felis. In luctus a lorem a ultrices. Duis viverra, odio eget molestie mattis, dui leo accumsan dui, interdum auctor velit elit tincidunt leo.",
+        photo: 'url(./assets/images/pedro.png)'
+    },
+    {
+        name: "Victor",
+        category: "Front-End",
+        description: "Tecnologia vai além de código ou componentes eletrônicos, se trata de tudo que a vida pode te oferecer de bom, seja ela lazer, diversão, pesssoas, união, etc. Por isso que eu me empenho todos os dias a dar isso a mim e minha família",
+        photo: 'url(./assets/images/victor.png)'
     },
 ]
-
 
 var membroAtual = 0;
 
@@ -38,6 +43,7 @@ function atualizarDados(membro) {
     nomeMembro.innerHTML = members[membro].name;
     categoria.innerHTML = members[membro].category;
     descricao.innerHTML = members[membro].description;
+    fotoMembroTeam.style.backgroundImage = members[membro].photo
 }
 
 atualizarDados(membroAtual)
