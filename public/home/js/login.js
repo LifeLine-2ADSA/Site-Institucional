66115a3f3407f148a7edd8d36855c64ab50655c6
GabrielGuedesSPTECH <gabriel.gmacedo@sptech.school>
2024-05-04 18:02:13 -0300

    Mudanças na estilização do login e adicionando cargo ao sessionStorage

--- public/home/js/login.js
+++ public/home/js/login.js
@@ -1,9 +1,45 @@
+const inputEmail = document.getElementById("email_input");
+const inputSenha = document.getElementById("senha_input");
+function todosErrados() {
+  inputEmail.classList.toggle("animacaoErro");
+  inputSenha.classList.toggle("animacaoErro");
+  setTimeout(() => {
+    alert("Campos vazios! Preencha!");
+    inputEmail.classList.toggle("animacaoErro");
+    inputSenha.classList.toggle("animacaoErro");
+  }, 750);
+}
+
+function senhaErrada() {
+  inputSenha.classList.toggle("animacaoErro");
+  setTimeout(() => {
+    alert("O campo senha não está preenchido");
+    inputSenha.classList.toggle("animacaoErro");
+  }, 750);
+}
+
+function emailErrado() {
+  inputEmail.classList.toggle("animacaoErro");
+  setTimeout(() => {
+    alert("O email inválido preencha novamente");
+    inputEmail.classList.toggle("animacaoErro");
+  }, 750);
+}
+
 function autenticar() {
   let emailVar = email_input.value;
   let senhaVar = senha_input.value;
-
-  if (emailVar == "" || senhaVar == "") {
-    alert("Campos vazios! Preencha!");
+  let regex =
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  console.log(regex.test(emailVar));
+  if (emailVar == "" && senhaVar == "") {
+    todosErrados();
+    return false;
+  } else if (senhaVar == "") {
+    senhaErrada();
+    return false;
+  } else if (emailVar == "" || !regex.test(emailVar)) {
+    emailErrado();
     return false;
   } else {
     fetch("/usuarios/autenticar", {
@@ -18,17 +54,15 @@ function autenticar() {
     })
       .then(function (resposta) {
         if (resposta.ok) {
-          console.log(resposta);
-
           resposta.json().then((json) => {
             sessionStorage.EMAIL_USUARIO = json.email;
             sessionStorage.NOME_USUARIO = json.nome;
             sessionStorage.ID_USUARIO = json.id;
+            sessionStorage.CARGO_USUARIO = json.cargo;
             sessionStorage.MAQUINAS_USUARIO = JSON.stringify(json.maquinas);
-
             setTimeout(function () {
               window.location = "././dashboard.html";
-            }, 1000); // apenas para exibir o loading
+            }, 20000); // apenas para exibir o loading
           });
         } else {
           alert("Houve um erro ao tentar realizar o login!");
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -28,6 +28,7 @@ function autenticar(req, res) {
                 email: resultadoAutenticar[0].email,
                 nome: resultadoAutenticar[0].nome,
                 senha: resultadoAutenticar[0].senha,
+                cargo: resultadoAutenticar[0].cargo,
                 maquinas: resultadoMaquinas,
               });
               // } else {
