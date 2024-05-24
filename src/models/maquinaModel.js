3384bd4bea5faa4f902ea7d32c72cbedbfb2a21a
vicg0 <victor.msantos@gmail.com>
2024-05-23 21:02:27 -0300

    filtrar maquinas por nome

--- src/models/maquinaModel.js
+++ src/models/maquinaModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function buscarMaquinasPorUsuario(idUsuario) {
   var instrucaoSql = `SELECT * FROM maquina JOIN usuario ON maquina.fkUsuario = usuario.idUsuario WHERE idUsuario = ${idUsuario};`;
 
-  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  console.log("Executando a instrução SQL maquina: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
